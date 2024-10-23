5a6278bc8e2db77c897af48a338a3a6627421242
Alonso Trejo <alonsotrejo1970@hotmail.com>
2024-10-23 10:58:54 -0600

    nuevos cambios

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -14,12 +15,33 @@ def login_user(request):
         if user is not None:
             login(request, user)
             messages.success(request, "You have been logged in!")
-            return redirect('cursos')
+            return redirect('home')
         else:
             messages.success(request, "There was an error at login in, please try again")
-            return redirect('home')
+            return render(request, 'users/login.html')
     else:
         return render(request, 'users/login.html')
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out, see you soon!")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'users/register.html', {'form': form})
+    
+    return render(request, 'users/register.html', {'form': form})
